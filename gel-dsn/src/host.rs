55e82c5f2b1e4d71e707f95a687d00a37d0bb5d3
Matt Mastracci <matthew@mastracci.com>
2025-03-12 11:00:16 -0600

    New DSN parser (#400)
    This is a rewrite of the gel-tokio DSN parser to make it more testable, and to not require an external environment to do so.
    
    The parser allows for a mock environment, filesystem and user profile setup to better test without requiring live filesystem modifications.
    
    The API of the Builder changed slightly, but efforts were made to maintain the older API. The build_env function was deprecated.
    
    Note that unnecessary async was removed from the builder. The likelihood of file access using this this DSN builder causing major stalls is very low. We can use classic blocking operations.
    
    This continues to be the easiest way to create a config:
    
    let config = Builder::default()::build().unwrap();
    
    We now add intentional ways to configure a client without the environment or filesystem (mainly for testing purposes):
    
    let config = Builder::default().host(...).port(...).without_system().build().unwrap();

--- gel-dsn/src/host.rs
+++ gel-dsn/src/host.rs
@@ -10,7 +10,8 @@ use serde::Serialize;
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[cfg_attr(feature = "serde", derive(Serialize))]
-pub enum HostTarget {
+#[allow(unused)]
+pub(crate) enum HostTarget {
     PostgreSQL,
     Gel,
     GelAdmin,
@@ -30,13 +31,13 @@ impl HostTarget {
 
 /// A pointer to a host and port which may be a hostname, IP address or unix
 /// socket.
-/// ```
-#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Clone, derive_more::Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[cfg_attr(feature = "serde", derive(Serialize))]
+#[debug("{}", self)]
 pub struct Host(pub(crate) HostType, pub(crate) u16, pub(crate) HostTarget);
 
 impl Host {
-    pub fn new(host: HostType, port: u16, host_target: HostTarget) -> Self {
+    pub(crate) fn new(host: HostType, port: u16, host_target: HostTarget) -> Self {
         Self(host, port, host_target)
     }
 
@@ -81,6 +82,31 @@ impl Host {
             self.0 .0,
             HostTypeInner::Path(_) | HostTypeInner::Abstract(_)
         )
+    }
+}
+
+impl std::fmt::Display for Host {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let port = self.1;
+        match &self.0 .0 {
+            HostTypeInner::Hostname(hostname) => write!(f, "{}:{}", hostname, port),
+            HostTypeInner::IP(ip, Some(interface)) => write!(f, "[{}%{}]:{}", ip, interface, port),
+            HostTypeInner::IP(ip, None) => write!(f, "[{}]:{}", ip, port),
+            HostTypeInner::Path(path) => {
+                if let Some(target_name) = self.2.target_name(port) {
+                    write!(f, "{}/{}", path.display(), target_name)
+                } else {
+                    write!(f, "{}", path.display())
+                }
+            }
+            HostTypeInner::Abstract(name) => {
+                if let Some(target_name) = self.2.target_name(port) {
+                    write!(f, "@{}/{}", name, target_name)
+                } else {
+                    write!(f, "@{}", name)
+                }
+            }
+        }
     }
 }
 
@@ -208,5 +234,21 @@ impl From<Ipv6Addr> for HostType {
 impl From<Ipv4Addr> for HostType {
     fn from(ip: Ipv4Addr) -> Self {
         HostType(HostTypeInner::IP(IpAddr::V4(ip), None))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::str::FromStr;
+
+    #[test]
+    fn test_host_display() {
+        let host = Host::new(
+            HostType::from_str("localhost").unwrap(),
+            5656,
+            HostTarget::Gel,
+        );
+        assert_eq!(host.to_string(), "localhost:5656");
     }
 }
