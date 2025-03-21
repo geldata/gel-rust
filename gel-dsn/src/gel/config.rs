c4fc546365b5c06f416ef2ee6e0ad67126876812
Matt Mastracci <matthew@mastracci.com>
2025-03-21 14:35:19 -0600

    .

--- gel-dsn/src/gel/config.rs
+++ gel-dsn/src/gel/config.rs
@@ -26,6 +26,7 @@ pub const DEFAULT_POOL_SIZE: usize = 10;
 pub const DEFAULT_HOST: &HostType = crate::host::LOCALHOST;
 pub const DEFAULT_PORT: u16 = 5656;
 pub const DEFAULT_USER: &str = "edgedb";
+pub const DEFAULT_BRANCH: DatabaseBranch = DatabaseBranch::Default;
 
 /// The result of building a [`Config`].
 pub struct ConfigResult {
