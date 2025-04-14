c2bbf09547cac328fb22b06042b56a6106d7aa0a
Matt Mastracci <matthew@mastracci.com>
2025-04-14 11:21:26 -0600

    gel-db

--- gel-db-proto/src/message_group.rs
+++ gel-db-proto/src/message_group.rs
@@ -85,8 +85,8 @@ pub use __message_group as message_group;
 /// Perform a match on a message.
 ///
 /// ```rust
-/// use db_proto::*;
-/// use db_proto::test_protocol::data::*;
+/// use gel_db_proto::*;
+/// use gel_db_proto::test_protocol::data::*;
 ///
 /// let buf = [b'?', 0, 0, 0, 4];
 /// match_message!(Message::new(&buf), Backend {
