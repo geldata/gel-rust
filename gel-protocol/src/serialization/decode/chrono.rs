7268e4ae96ab54754d35e0922dee1141e41399ed
Matt Mastracci <matthew@mastracci.com>
2025-06-11 12:09:02 -0600

    .

--- gel-protocol/src/serialization/decode/chrono.rs
+++ gel-protocol/src/serialization/decode/chrono.rs
@@ -2,25 +2,25 @@ use crate::errors::DecodeError;
 use crate::serialization::decode::raw_scalar::RawCodec;
 use chrono::{DateTime, NaiveDate, NaiveDateTime, NaiveTime, Utc};
 
-impl<'t> RawCodec<'t> for DateTime<Utc> {
+impl RawCodec<'_> for DateTime<Utc> {
     fn decode(buf: &[u8]) -> Result<Self, DecodeError> {
         crate::model::Datetime::decode(buf).map(Into::into)
     }
 }
 
-impl<'t> RawCodec<'t> for NaiveDateTime {
+impl RawCodec<'_> for NaiveDateTime {
     fn decode(buf: &[u8]) -> Result<Self, DecodeError> {
         crate::model::LocalDatetime::decode(buf).map(Into::into)
     }
 }
 
-impl<'t> RawCodec<'t> for NaiveDate {
+impl RawCodec<'_> for NaiveDate {
     fn decode(buf: &[u8]) -> Result<Self, DecodeError> {
         crate::model::LocalDate::decode(buf).map(Into::into)
     }
 }
 
-impl<'t> RawCodec<'t> for NaiveTime {
+impl RawCodec<'_> for NaiveTime {
     fn decode(buf: &[u8]) -> Result<Self, DecodeError> {
         crate::model::LocalTime::decode(buf).map(Into::into)
     }
