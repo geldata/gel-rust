71772345c71f5e22523927c2011ac087bd49bda5
Aljaž Mur Eržen <aljaz@erzen.si>
2025-08-21 10:39:13 +0200

    Gel captive log file path

--- gel-captive/src/lib.rs
+++ gel-captive/src/lib.rs
@@ -1,19 +1,29 @@
 mod server;
 mod utils;
 
+use std::path;
+
 pub use server::{ServerInfo, ServerProcess};
 
 pub struct ServerBuilder {
-    // TODO
+    log_file_path: Option<path::PathBuf>
 }
 
 impl ServerBuilder {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            log_file_path: None,
+        }
     }
 
     pub fn start(self) -> ServerProcess {
-        ServerProcess::start()
+        ServerProcess::start(self)
+    }
+
+    /// Sets filename of the server output log.
+    /// Defaults to a random temp file.
+    pub fn log_file_path(&mut self, path: Option<path::PathBuf>) {
+        self.log_file_path = path;
     }
 }
 
